875168d21d398bad6d2a20219bf0f5cd51a97348
mouyong <my24251325@gmail.com>
2023-10-13 16:14:31 +0800

    chore: 修复无法自动添加 key 的问题

---
+++ app/Models/Traits/ConfigServiceTrait.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Plugins\LaravelConfig\Models\Traits;
+
+use Plugins\LaravelConfig\Models\Config;
+use Plugins\MarketManager\Utils\LaravelCache;
+
+/**
+ * @mixin Config
+ */
+trait ConfigServiceTrait
+{
+    public static function findConfig(string $itemKey = null, ?string $itemTag, array $where = []): null|Config
+    {
+        if (empty($itemKey)) {
+            return null;
+        }
+        
+        if ($itemKey) {
+            $where['item_key'] = $itemKey;
+        }
+
+        if ($itemTag) {
+            $where['item_tag'] = $itemTag;
+        }
+
+        return Config::query()->where($where)->first();
+    }
+
+    public static function getValueByKey(string $itemKey, ?string $itemTag = null, array $where = [])
+    {
+        $cacheKey = Config::CACHE_KEY_PREFIX . $itemKey;
+
+        $config = LaravelCache::remember($cacheKey, function () use ($itemKey, $itemTag, $where) {
+            $where['item_key'] = $itemKey;
+            if ($itemTag) {
+                $where['item_tag'] = $itemTag;
+            }
+
+            return Config::query()->where($where)->first();
+        });
+
+        return $config?->item_value;
+    }
+
+    public static function getValueByKeys(array $itemKeys, ?string $itemTag = null, array $where = []): array
+    {
+        $data = [];
+
+        foreach ($itemKeys as $index => $itemKey) {
+            $value = LaravelCache::get($itemKey);
+            if ($value) {
+                unset($itemKeys[$index]);
+
+                $data[$itemKey] = $value;
+            }
+        }
+
+        // 所有数据已全部查出
+        if (count($data) === count($itemKeys)) {
+            return $data;
+        }
+
+        // 拼接额外查询条件
+        if ($itemTag) {
+            $where['item_tag'] = $itemTag;
+        }
+
+        // 已查出部分缓存中的 key，还有部分 key 需要查询
+        foreach ($itemKeys as $index => $itemKey) {
+            $data[$itemKey] = static::getValueByKey($itemKey);
+        }
+
+        return $data;
+    }
+
+    public static function forgetCache(?string $itemKey)
+    {
+        $cacheKey = Config::CACHE_KEY_PREFIX . $itemKey;
+        LaravelCache::forget($cacheKey);
+    }
+}
---
+++ app/Utilities/ConfigUtility.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Plugins\LaravelConfig\Utilities;
+
+use Plugins\LaravelConfig\Models\Config;
+use Plugins\LaravelConfig\Helpers\ConfigHelper;
+
+class ConfigUtility
+{
+    public static function updateConfigs(array $keys = [], ?string $itemTag = null, array $where = [])
+    {
+        $itemKeys = $keys;
+
+        foreach ($itemKeys as $itemKey) {
+            if ($itemTag) {
+                $where['item_tag'] = $itemTag;
+            }
+
+            $config = Config::findConfig($itemKey, $itemTag);
+            if ($config) {
+                $config->item_value = \request($itemKey);
+                $config->save();
+    
+                Config::forgetCache($config->item_key);
+            } else {
+                $itemValue = \request($itemKey);
+                $itemType = match(true) {
+                    default => 'string',
+                    is_string($itemValue) => 'string',
+                    is_array($itemValue) => 'json',
+                };
+
+                ConfigUtility::addFresnsConfigItems([
+                    [
+                        'item_tag' => $itemTag,
+                        'item_key' => $itemKey,
+                        'item_type' => $itemType,
+                        'item_value' => $itemValue,
+                    ],
+                ]);
+            }
+        }
+
+        $result = ConfigHelper::fresnsConfigByItemKeys($itemKeys, $itemTag);
+
+        return $result;
+    }
+
+    public static function addFresnsConfigItems(array $fresnsConfigItems): void
+    {
+        foreach ($fresnsConfigItems as $item) {
+            $config = Config::where('item_key', $item['item_key'])->first();
+            if (empty($config)) {
+                Config::create($item);
+
+                if ($item['is_multilingual'] ?? null) {
+                    $fresnsLangItems = [
+                        'table_name' => 'configs',
+                        'table_column' => 'item_value',
+                        'table_id' => null,
+                        'table_key' => $item['item_key'],
+                        'language_values' => $item['language_values'] ?? [],
+                    ];
+
+                    ConfigUtility::changeFresnsLanguageItems($fresnsLangItems);
+                }
+            }
+        }
+    }
+
+    // remove config items
+    public static function removeFresnsConfigItems(array $fresnsConfigKeys): void
+    {
+        foreach ($fresnsConfigKeys as $item) {
+            $config = Config::findConfig($item['item_key'], $item['item_tag']);
+
+            if ($config?->is_custom == 1 && $config?->is_multilingual == 1) {
+                // Language::where('table_name', 'configs')->where('table_column', 'item_value')->where('table_key', $key)->forceDelete();
+            }
+
+            $config?->forceDelete();
+
+            Config::forgetCache($config?->item_key);
+        }
+    }
+
+    // change config items
+    public static function changeFresnsConfigItems(array $fresnsConfigItems): void
+    {
+        foreach ($fresnsConfigItems as $item) {
+            Config::updateOrCreate([
+                'item_key' => $item['item_key'],
+            ],
+                collect($item)->only('item_key', 'item_value', 'item_type', 'item_tag', 'is_multilingual', 'is_api')->toArray()
+            );
+
+            if ($item['is_multilingual'] ?? null) {
+                $fresnsLangItems = [
+                    'table_name' => 'configs',
+                    'table_column' => 'item_value',
+                    'table_id' => null,
+                    'table_key' => $item['item_key'],
+                    'language_values' => $item['language_values'] ?? [],
+                ];
+
+                ConfigUtility::changeFresnsLanguageItems($fresnsLangItems);
+            }
+
+            Config::forgetCache($item['item_key']);
+        }
+    }
+
+    // change language items
+    public static function changeFresnsLanguageItems($fresnsLangItems): void
+    {
+        foreach ($fresnsLangItems['language_values'] ?? [] as $key => $value) {
+            $item = $fresnsLangItems;
+            $item['lang_tag'] = $key;
+            $item['lang_content'] = $value;
+
+            // use collect()->only() instead of unset($item['language_values'])
+            // unset($item['language_values']);
+
+            // Language::updateOrCreate($item);
+        }
+    }
+}
